2ddccf9979c266063bf5203177dd8dde8ead49dd
CloudLunar <CloudLunar@qq.com>
2016-04-23 18:41:28 +0800

    duoshuo bug

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,7 +14,6 @@ $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gi
 $(function() {
   $(".content").fitVids();
 });
-	$("body").lazyload({effect : "fadeIn"});
 // All others
 $(document).ready(function() {
     // zoom in/zoom out animations
